7ea848df238774bb3641fa348af20667636c20b8
liguirong <1013868552@qq.com>
2019-01-16 15:55:48 +0800

    add baseTable

--- src/views/Users/index.js
+++ src/views/Users/index.js
@@ -1,10 +1,21 @@
 
 import React, { Component } from 'react';
+import BaseTable from '../../components/BaseTable/BaseTable';
+
+const data = [];
+for (let i = 0; i < 100; i++) {
+  data.push({
+      key: i.toString(),
+      name: `Edrward ${i}`,
+      age: 32,
+      address: `London Park no. ${i}`,
+  });
+}
 
 class Users extends Component {
   render() {
     return (
-        <p>功能开发中，尽情期待</p>
+      <BaseTable dataSource={data}/>
     );
   }
 }
